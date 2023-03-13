f6fc0ef9b9d8a6fe409f3eff5f0ae0d9257ea2f9
lu0554 <127291632+lu0554@users.noreply.github.com>
2023-03-13 11:55:45 -0400

    model

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 sns.set()
 
-def education_depression_relationship(data: pd.DataFrame):
-    data = data[["education_level", "depressed"]]
-    education_level = data[["education_level"][0]]
-    depression_status = data[["depressed"][0]]
-    plt.hist(education_level, bins=40, color='green', alpha=0.5, label='Non-depressed')
-    plt.hist([education_level[i] for i in range(len(depression_status)) if depression_status[i] == 1],
-             bins=40, color='blue', alpha=0.5, label='Depressed')
-    plt.xlabel("Education Level")
-    plt.ylabel("Number of people")
-    plt.tight_layout()
-    plt.savefig("education.png")
-    percentage = get_education_percent(education_level, depression_status)
-    print(percentage)
-    sns.lineplot(data=percentage, x="education_level", y="percentage")
-    plt.savefig("percent.png")
 
 def money_depression_relationship(data: pd.DataFrame):
     data = data[["gained_asset", "depressed", "durable_asset", "save_asset"]]
@@ -50,36 +38,23 @@ def money_depression_relationship(data: pd.DataFrame):
     ax4.set_ylabel('Depressed')
     plt.tight_layout()
     plt.savefig("asset_depression.png")
-    fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(nrows=2, ncols=2)
+    fig, ax = plt.subplots(3)
     data = get_precent(gained_asset, depression_status)
-    sns.lineplot(data=data, x="income", y="percentage", ax=ax1)
+    sns.lineplot(data=data, x="income", y="percentage", ax=ax[0])
     ax1.set_xlabel('Gained asset')
     ax1.set_ylabel('Depressed Percentage')
     data = get_precent(durable_asset, depression_status)
-    sns.lineplot(data=data, x="income", y="percentage", ax=ax2)
+    sns.lineplot(data=data, x="income", y="percentage", ax=ax[1])
     ax2.set_xlabel('Durable asset')
     ax2.set_ylabel('Depressed Percentage')
     data = get_precent(save_asset, depression_status)
-    sns.lineplot(data=data, x="income", y="percentage", ax=ax3)
+    sns.lineplot(data=data, x="income", y="percentage", ax=ax[2])
     ax3.set_xlabel('Save asset')
     ax3.set_ylabel('Depressed Percentage')
     plt.tight_layout()
     plt.savefig("percentage_asset")
 
-def get_education_percent(data, depression):
-    depression_num = [0] * 20
-    total_num = [0] * 20
-    percentage = [0] * 20
-    for i in range(len(data)):
-        if depression[i] == 1:
-            depression_num[data[i]] += 1
-        total_num[data[i]] += 1
-    for i in range(len(depression_num)):
-        if total_num[i] != 0:
-            percentage[i] = depression_num[i] / total_num[i]
-    data = {'percentage': percentage,
-            'education_level': list(range(1, 21))}
-    return data
+
 def get_precent(data, depression):
     depression_num = [0, 0, 0, 0, 0]
     total_num = [0, 0, 0, 0, 0]
@@ -95,10 +70,23 @@ def get_precent(data, depression):
     return data
 
 
+def logistic_model(df):
+    df = df.dropna()
+    X = df.loc[:, df.columns != 'depressed']
+    X = pd.get_dummies(X)
+    y = df.depressed
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print("Accuracy score:", accuracy_score(y_test, y_pred))
+    print("Precision score:", precision_score(y_test, y_pred))
+
+
 def main():
-    data = pd.read_csv('dateset/b_depressed.csv')
+    data = pd.read_csv("dateset/b_depressed.csv")
     money_depression_relationship(data)
-    education_depression_relationship(data)
+    logistic_model(data)
 
 
 if __name__ == '__main__':
